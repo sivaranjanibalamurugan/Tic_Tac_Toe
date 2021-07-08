ce62c0caebb2a2c47f0049e8b4271069799f5592
unknown <sivaranjanib99@gmail.com>
2021-07-08 13:40:37 +0530

    [siva] display the board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,11 +8,11 @@ namespace TicTacToe
         {
             Console.WriteLine("Welcome to Tic Tac Toe!!");
             TicTacToe ticTacToe = new TicTacToe();
-            
             char[] board = ticTacToe.CreateBoard();
             char playerChoice = ticTacToe.SelectOption();
             char computerChoice = playerChoice == 'X' ? 'O' : 'X';
             Console.WriteLine("Player choice:{0} Computer Choice:{1}", playerChoice, computerChoice);
+            ticTacToe.DisplayBoard(board);
         }
     }
 }
--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -7,9 +7,12 @@ using System.Threading.Tasks;
 namespace TicTacToe
 {
     class TicTacToe
-    {
+    { 
+         public const int ROW = 3;
+        public const int COLUMN = 3;
+    
         public char[] board;
-        public char []CreateBoard()
+        public char[] CreateBoard()
         {
 
 
@@ -25,8 +28,38 @@ namespace TicTacToe
         {
             Console.WriteLine("Enter your choice as X or O");
             char choice = Convert.ToChar(Console.ReadLine());
-            return choice;
+
+            if (char.ToUpper(choice) != 'X' && char.ToUpper(choice) != 'O')
+            {
+                Console.WriteLine("Enter either X or O");
+                choice = SelectOption();
+            }
+
+            return char.ToUpper(choice);
+
+        }
+        public void DisplayBoard(char[] board)
+        {
+            int row = 0;
+            int column = 0;
+            int position = 1;
+            Console.WriteLine("-------------");
+            while (row != ROW)
+            {
+                column = 0;
+                while (column != COLUMN)
+                {
+                    Console.Write(" {0} |", board[position]);
+                    position++;
+                    column++;
+                }
+                Console.WriteLine();
+                Console.WriteLine("-------------");
+                row++;
+            }
         }
 
     }
+
 }
+
