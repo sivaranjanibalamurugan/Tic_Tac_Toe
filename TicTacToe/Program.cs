bc0c80daa9b7385b6c9658a354d33566960bc0de
unknown <sivaranjanib99@gmail.com>
2021-07-08 12:23:09 +0530

    [siva] selecting the option

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe!!");
+            TicTacToe ticTacToe = new TicTacToe();
+            
+            char[] board = ticTacToe.CreateBoard();
+            char playerChoice = ticTacToe.SelectOption();
+            char computerChoice = playerChoice == 'X' ? 'O' : 'X';
+            Console.WriteLine("Player choice:{0} Computer Choice:{1}", playerChoice, computerChoice);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class TicTacToe
+    {
+        public char[] board;
+        public char []CreateBoard()
+        {
+
+
+            board = new char[10];
+            for (int i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+            Console.WriteLine("Empty board created");
+            return board;
+        }
+        public char SelectOption()
+        {
+            Console.WriteLine("Enter your choice as X or O");
+            char choice = Convert.ToChar(Console.ReadLine());
+            return choice;
+        }
+
+    }
+}
